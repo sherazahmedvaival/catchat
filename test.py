68bc1ab35cfff2dc20f2c78f5ed2ad2cb78a139a
Sheraz Ahmed <sheraz.ahmed@vaival.com>
2025-05-02 17:19:13 +0500

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ os.system(command)
 
 user_input = input("Enter your username: ")
 query = "SELECT * FROM users WHERE username = '" + user_input + "';"
-execute_query(query)  # This can be exploited!
+execute_query(query)  # This can be exploited!!
